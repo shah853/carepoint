a72cadaaeb8f5e17b07544f263eaea6b53801456
Muhammad Shah <m.shahafridi52@gmail.com>
2026-08-26 10:36:48 -0700

    Fix MongoDB API doctors products deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
 require('dotenv').config();
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/carepoint';
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      throw new Error('MONGO_URI is missing from .env');
+    }
 
     await mongoose.connect(mongoURI, {
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 15000,
+      connectTimeoutMS: 15000,
+      socketTimeoutMS: 45000,
     });
 
     console.log('MongoDB connected successfully');
