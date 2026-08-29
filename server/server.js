8b540ddba11b96d833e42196b67d72d7e3908ca1
Muhammad Shah <m.shahafridi52@gmail.com>
2026-08-29 00:50:28 -0700

    Load Atlas environment consistently

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({
+  path: path.resolve(__dirname, '.env'),
+});
 
 const connectDB = require('./config/db');
 const { isDatabaseReady } = connectDB;
